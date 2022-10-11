dc900fab6e375d56a2ef4b6ed06dfdf5d883e958
Rob Jesson <robjesson73@gmail.com>
2022-10-11 11:19:35 +0100

    Updated code to display a longer multi-line message

--- basics/output/multiline_message.py
+++ basics/output/multiline_message.py
@@ -1,3 +1,10 @@
 # Display message to the standard output
 print("System Failure Imminent!")
 print("System reboot has been initiated...")
+print("")
+print("...rebooting sensory systems")
+print("...rebooting output motors")
+print("...rebooting hover engine")
+print("")
+print("system online")
+
