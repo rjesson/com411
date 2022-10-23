d1efbbe8044f0400d0ddb84ad8bc1394cef0973f
Rob Jesson <robjesson73@gmail.com>
2022-10-23 08:52:05 +0100

    Added code to demonstrate the use of one loop nested is another.

---
+++ basics/repetitions/nested_loop/nested.py
@@ -0,0 +1,17 @@
+# Display message
+print("How many rows should I have?")
+
+# Read user's input
+rows = int(input())
+
+# Display message
+print("How many columns should I have?")
+
+# Read user's input
+columns = int(input())
+
+# Display grid
+for row in range(0, rows, 1):
+    for column in range(0, columns, 1):
+        print(":-)", end="")
+    print()
