677e87f6aaa0f5ab7a8fba0c09f5094aa9f23b55
Rob Jesson <robjesson73@gmail.com>
2022-10-28 11:24:15 +0100

    Added code to demonstrate the import and use of a module.

---
+++ basics/modules/guess_the_number.py
@@ -0,0 +1,30 @@
+import random
+
+# Ask user for minimum value
+print("Please enter the minimum value")
+min_value = int(input())
+
+# Ask user for maximum value
+print("Please enter the maximum value")
+max_value = int(input())
+
+random_number = random.randrange(min_value, max_value)
+
+print(f"I am thinking of a number between {min_value} and {max_value}.")
+print("Can you guess what it is?")
+
+guessed_correctly = False
+
+while not guessed_correctly:
+    print("Please enter a number:")
+    guess = int(input())
+
+    if guess == random_number:
+        print("Congratulations! You guessed the correct answer!")
+        guessed_correctly = True
+    elif guess < random_number:
+        print("Guess higher")
+    else:
+        print("Guess lower")
+
+print("Game over!")
