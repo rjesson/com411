de6045ab320fb1b450833f2bb6c9be5804cc5496
Rob Jesson <6jessr63@solent.ac.uk>
2022-11-08 11:01:09 +0000

    Added code to read a JSON file

--- data/files/json/read_json.py
+++ data/files/json/read_json.py
@@ -6,17 +6,17 @@ def read (file_path):
         # load json file and store in a variable
         data = json.load(json_file)
         # extract city and store in a variable
-        city_name = data("city")
+        city_name = data["city"]
         # display name of the city
         print(f"City name: {city_name}")
         # extract population and store in a variable
-        population_size = data("population")
+        population_size = data["population"]
         # display population
         print(f"Population size: {population_size}")
         # extract stats for each bot
-        for bot in data("bots"):
-            name = bot('name')
-            stats = bot ('stats')
+        for bot in data["bots"]:
+            name = bot['name']
+            stats = bot['stats']
             print(f"{name} has the following stats: {stats}")
 
 def run():
