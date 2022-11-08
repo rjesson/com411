0df8ba12bbe895d940f0662da89d40bf7941cdf0
Rob Jesson <6jessr63@solent.ac.uk>
2022-11-08 14:07:11 +0000

    Added code to demonstrate a simple list.

---
+++ data/lists/simple_list.py
@@ -0,0 +1,11 @@
+# Define function to create a list of directions
+def directions():
+    directions = ["Move Forward", "Move Backwards", "Turn Left", "Turn Right"]
+    return directions
+
+def run():
+    print(directions())
+
+if __name__ == "__main__":
+    run()
+
